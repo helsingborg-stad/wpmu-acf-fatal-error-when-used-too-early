54cf6039015bff567c264ebe73d2899c878454ab
GitHub Action <>
2026-09-18 13:33:50 +0000

    build: update package file versions to 0.2.0

--- wpmu-acf-fatal-error-when-used-too-early.php
+++ wpmu-acf-fatal-error-when-used-too-early.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: WPMU ACF Fatal Error When Used Too Early
  * Description: Throws a descriptive exception when an ACF field is accessed before WordPress has initialized.
- * Version: 0.1.2
+ * Version: 0.2.0
  * Author:      Helsingborgs stad
  */
 
